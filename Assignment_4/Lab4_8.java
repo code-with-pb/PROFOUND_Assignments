a8287aa90c1288d4b7b2c8f61ea7118212a5bfb2
code-with-pb <98020094+code-with-pb@users.noreply.github.com>
2024-02-17 17:06:17 +0530

    Size Min Max Value of Primitive data Type

---
+++ Assignment_4/Lab4_8.java
@@ -0,0 +1,16 @@
+
+public class Lab4_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("s.No. \t data type \t Size \t Min. Value \t\t Max. Value \t");
+		System.out.println("1\t Byte\t\t"+ Byte.SIZE+" \t "+Byte.MIN_VALUE+" \t\t\t "+Byte.MAX_VALUE);
+		System.out.println("1\t Short\t\t"+ Short.SIZE+" \t "+Short.MIN_VALUE+" \t\t\t "+Short.MAX_VALUE);
+		System.out.println("1\t Byte\t\t"+ Integer.SIZE+" \t "+Integer.MIN_VALUE+" \t\t\t "+Integer.MAX_VALUE);
+		System.out.println("1\t Byte\t\t"+ Float.SIZE+" \t "+Float.MIN_VALUE+" \t\t\t "+Float.MAX_VALUE);
+		System.out.println("1\t Byte\t\t"+ Long.SIZE+" \t "+Long.MIN_VALUE+" \t\t\t "+Long.MAX_VALUE);
+		System.out.println("1\t Byte\t\t"+ Double.SIZE+" \t "+Double.MIN_VALUE+" \t\t\t "+Double.MAX_VALUE);
+		System.out.println("1\t Byte\t\t"+ Character.SIZE+" \t "+Character.MIN_VALUE+" \t\t\t "+Character.MAX_VALUE);
+	}
+
+}
