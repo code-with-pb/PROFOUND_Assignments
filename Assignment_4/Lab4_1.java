5403f9a0c4c86fcee0c894bad9d364c194f51b3d
code-with-pb <98020094+code-with-pb@users.noreply.github.com>
2024-02-17 15:59:49 +0530

    Update Lab4_1.java

--- Assignment_4/Lab4_1.java
+++ Assignment_4/Lab4_1.java
@@ -3,7 +3,27 @@ public class Lab4_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	int id;
+	String nm;
+	String dob;
+	Lab4_1(){
+		System.out.println("deafult Constructor ");
+	}
+	Lab4_1(int id,String nm, String dob){
+		this.id = id;
+		this.nm = nm;
+		this.dob = dob;
+	}
+	public String toString() {
+		return id+" "+nm+" "+dob;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lab4_1 l = new Lab4_1();
+		
+		Lab4_1 l1 = new Lab4_1(101,"Akash","2000/05/20");
+		
+		System.out.println(l1.toString());
 	}
 
 }
