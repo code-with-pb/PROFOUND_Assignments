debc7e7cd09cd2d6df15df36ce16159ca3661dbd
code-with-pb <98020094+code-with-pb@users.noreply.github.com>
2024-02-19 17:36:06 +0530

    Update Lab6_1.java

--- Assignment_6/Lab6_1.java
+++ Assignment_6/Lab6_1.java
@@ -1,4 +1,37 @@
 
+abstract class Instrument{
+	public abstract void play();
+}
+class Piano extends Instrument{
+	public void play() {
+		System.out.println("Piano is playing tan tan tan");
+	}
+}
+class Flute extends Instrument{
+	public void play() {
+		System.out.println("Flute is playing toot toot toot");
+	}
+}
+class Guitar extends Instrument{
+	public void play() {
+		System.out.println("Guitar is playing tin tin tin");
+	}
+}
 public class Lab6_1 {
-
+	public static void main(String args[]) {
+		Instrument i[];
+		
+		i = new Instrument[10];
+		
+		//i[] = new Instrument(10);
+		
+		i[0] = new Piano();
+		i[0].play();
+		
+		i[1] = new Flute();
+		i[1].play();
+		
+		i[2] = new Guitar();
+		i[2].play();
+	}
 }
