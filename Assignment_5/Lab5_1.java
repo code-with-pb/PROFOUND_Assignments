846b2cdbc76eb169c574f463411a2e3422c62812
code-with-pb <98020094+code-with-pb@users.noreply.github.com>
2024-02-19 16:15:54 +0530

    Update Lab5_1.java

--- Assignment_5/Lab5_1.java
+++ Assignment_5/Lab5_1.java
@@ -1,9 +1,24 @@
-
+class ame{
+	ame(){
+		System.out.println("Game Class");
+	}
+}
+class boardgame extends ame{
+	boardgame(){
+		System.out.println("Board Game Class");
+	}
+}
+class chess extends boardgame{
+	chess(){
+		System.out.println("Chess Game Class");
+	}
+}
 public class Lab5_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		chess c = new chess();
+		
 	}
 
 }
