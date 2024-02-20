8086d1f2608282b62d524204531ecec082f867ed
code-with-pb <98020094+code-with-pb@users.noreply.github.com>
2024-02-20 16:47:19 +0530

    Package-2_Acess_Specifer_use

---
+++ Assignment_7/Lab7_4.java
@@ -0,0 +1,15 @@
+import Pack2.*;
+
+public class Lab7_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Batch b = new Batch(2017);
+		
+		b.dis();
+		// b.display();  cannot Accessed because we had used protected access specifier
+		
+		// b.show();  cannot Accessed because we had used private access specifier
+	}
+
+}
