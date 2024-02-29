3cafac7f882aee620642223b03c8a09d974fe8d6
code-with-pb <98020094+code-with-pb@users.noreply.github.com>
2024-02-29 17:36:32 +0530

    Update Lab14_1.java

--- Assignment_14/Lab14_1.java
+++ Assignment_14/Lab14_1.java
@@ -2,8 +2,8 @@
 public class Lab14_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stu
+		ThreadMethod tm = new ThreadMethod();
 	}
 
 }
